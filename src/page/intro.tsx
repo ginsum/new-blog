c2295c3f8ae6c42051f5a9acb94ba891cbfe9b95
ginsum <immime12697@gmail.com>
2023-01-03 22:15:17 +0900

    UPDATE: react-spring 설치

--- src/page/intro.tsx
+++ src/page/intro.tsx
@@ -1,7 +1,71 @@
-const Intro = () => (
-  <div>
-    <div>Intro</div>
-  </div>
-);
+import { useRef, useState } from 'react';
+import { useTrail, animated } from '@react-spring/web';
+
+const items = ['H', 'E', 'L', 'L', 'O', 'W', 'O', 'R', 'L', 'D'];
+const itemsFront = ['🔥', '🔥', 'L', '🔥', '🔥', '🔥', '🔥', 'R', '🔥', '🔥'];
+
+const Intro = () => {
+  const [isOpen, setIseOpen] = useState<boolean>(false);
+  const [trail, api] = useTrail(items.length, () => ({
+    rotateX: 0,
+  }));
+
+  const isFlipped = useRef(false);
+
+  const handleClick = () => {
+    if (isFlipped.current) {
+      api.start({
+        rotateX: 0,
+      });
+      isFlipped.current = false;
+    } else {
+      api.start({
+        rotateX: 180,
+      });
+      isFlipped.current = true;
+    }
+    setIseOpen(!isOpen);
+  };
+
+  return (
+    <div className="flex flex-col w-full h-screen justify-center items-center">
+      <div className="flex flex-wrap w-96" onClick={handleClick}>
+        {trail.map(({ rotateX }, i) => (
+          <div
+            key={i}
+            className="flex justify-center items-center w-16 h-16 relative ml-2 mb-2 text-3xl bg-rose-800 text-white"
+          >
+            <animated.div
+              key={items[i]}
+              style={{
+                transform: rotateX.to(
+                  (val) => `perspective(600px) rotateX(${val}deg)`
+                ),
+                transformStyle: 'preserve-3d',
+                backfaceVisibility: 'hidden',
+              }}
+              className="absolute"
+            >
+              {itemsFront[i]}
+            </animated.div>
+            <animated.div
+              style={{
+                transform: rotateX.to(
+                  (val) => `perspective(600px) rotateX(${180 - val}deg)`
+                ),
+                transformStyle: 'preserve-3d',
+                backfaceVisibility: 'hidden',
+              }}
+              className="absolute"
+            >
+              {items[i]}
+            </animated.div>
+          </div>
+        ))}
+      </div>
+      <div className="h-4">{isOpen ? `Blog ->` : ''}</div>
+    </div>
+  );
+};
 
 export default Intro;
--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,10 +1,12 @@
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route } from 'react-router-dom';
 
-import Intro from "../page/intro";
-import Resume from "../page/resume";
-import BlogList from "../page/blog/list";
-import BlogDetail from "../page/blog/detail";
-import Write from "../page/blog/write";
+import Intro from '../page/intro';
+
+import Resume from '../page/resume';
+import BlogList from '../page/blog/list';
+import BlogDetail from '../page/blog/detail';
+import Write from '../page/blog/write';
+import Login from '../page/login';
 
 const Root = () => {
   return (
@@ -14,6 +16,7 @@ const Root = () => {
       <Route path="blog" element={<BlogList />} />
       <Route path="blog/:id" element={<BlogDetail />} />
       <Route path="write" element={<Write />} />
+      <Route path="login" element={<Login />} />
     </Routes>
   );
 };
