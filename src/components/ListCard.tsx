5b8f8d0871b3bfbcfc3386e88e5003e084ccd059
ginsum <immime12607@gmail.com>
2023-01-10 18:21:36 +0900

    FIX: 블로그 목록 카드 미리 보기 수정

--- src/components/ListCard.tsx
+++ src/components/ListCard.tsx
@@ -1,4 +1,4 @@
-import { useNavigate } from 'react-router-dom';
+import { useNavigate } from "react-router-dom";
 
 const ListCard = ({
   title,
@@ -12,24 +12,26 @@ const ListCard = ({
   date: string;
 }) => {
   let navigate = useNavigate();
-  const contentSlice = `${content.substring(0, 90)}...`;
 
   return (
     <div
-      className="p-8 my-2 cursor-pointer border border-gray-200 rounded-2xl shadow"
+      className="px-8 pt-7 pb-8 my-2 cursor-pointer border border-zinc-200 rounded-2xl shadow"
       onClick={() => navigate(id)}
     >
-      <div className="flex justify-between items-end pb-2 border-b border-slate-300">
+      <div className="flex justify-between items-end pb-2 border-b border-zinc-300">
         <div className="text-xl font-bold text-blue-800">{title}</div>
-        <div className="text-sm text-gray-600">{date}</div>
+        <div className="text-sm text-zinc-600">{date}</div>
       </div>
       <div
-        className="mt-6 text-sm text-gray-500"
-        dangerouslySetInnerHTML={{
-          __html: contentSlice,
+        className="w-[524px] h-14 mt-4 text-sm text-zinc-500 overflow-hidden"
+        style={{
+          display: "-webkit-box",
+          WebkitBoxOrient: "vertical",
+          WebkitLineClamp: 4,
         }}
-      />
-      {/* <div className="text-slate-600">{content}</div> */}
+      >
+        {content}
+      </div>
     </div>
   );
 };
