c883e4ced4047c4e1d03224625d5a75821708e4f
ginsum <immime12697@gmail.com>
2023-01-18 00:12:46 +0900

    UPDATE: 랜딩페이지 테스트

--- src/page/intro.tsx
+++ src/page/intro.tsx
@@ -1,6 +1,12 @@
-import { useRef, useState } from 'react';
-import { Link, useNavigate } from 'react-router-dom';
-import { useTrail, animated } from '@react-spring/web';
+import { useEffect, useRef, useState, ReactNode } from 'react';
+import { useNavigate } from 'react-router-dom';
+import {
+  AnimatedProps,
+  useTrail,
+  animated,
+  useSpringRef,
+  useTransition,
+} from '@react-spring/web';
 
 const itemsFront = [
   '<',
@@ -16,17 +22,47 @@ const itemsFront = [
   'D',
   '/>',
 ];
-const items = ['C', 'L', 'I', 'C', 'K', '', '', '', '', 'M', 'E', '->'];
+const items = ['C', 'L', 'I', 'C', 'K', '', '', 'H', 'E', 'R', 'E', '->'];
+
+const pages: ((
+  props: AnimatedProps<{ children: ReactNode }>
+) => React.ReactElement)[] = [
+  ({ children }) => (
+    <animated.div className="flex flex-col w-full h-screen justify-center items-center bg-blue-200">
+      {children}
+    </animated.div>
+  ),
+  ({ children }) => (
+    <animated.div className="flex flex-col w-full h-screen justify-center items-center bg-blue-300">
+      {children}
+    </animated.div>
+  ),
+];
 
 const Intro = () => {
+  const [index, setIndex] = useState(0);
   const [trail, api] = useTrail(items.length, () => ({
     rotateX: 0,
   }));
 
   const navigate = useNavigate();
   const isFlipped = useRef(false);
+  const transRef = useSpringRef();
+
+  const transitions = useTransition(index, {
+    ref: transRef,
+    keys: null,
+    from: { opacity: 0, transform: 'translate3d(100%,0,0)' },
+    enter: { opacity: 1, transform: 'translate3d(0%,0,0)' },
+    // leave: { opacity: 0, transform: 'translate3d(-50%,0,0)' },
+  });
+
+  useEffect(() => {
+    transRef.start();
+  }, [index]);
 
   const handleClick = () => {
+    setIndex((state) => (state + 1) % 5);
     if (isFlipped.current) {
       navigate('/blog');
     } else {
@@ -39,43 +75,52 @@ const Intro = () => {
 
   return (
     <div className="flex flex-col w-full h-screen justify-center items-center">
-      <div
-        className="flex flex-wrap justify-center w-[300px] md:w-[500px] mb-20"
-        onClick={handleClick}
-      >
-        {trail.map(({ rotateX }, i) => (
-          <div
-            key={i}
-            className="flex justify-center items-center w-12 h-12 md:w-16 md:h-16 relative ml-0.5 md:ml-2 mb-2 text-3xl bg-blue-200 text-white rounded-full"
-          >
-            <animated.div
-              key={items[i]}
-              style={{
-                transform: rotateX.to(
-                  (val) => `perspective(600px) rotateX(${val}deg)`
-                ),
-                transformStyle: 'preserve-3d',
-                backfaceVisibility: 'hidden',
-              }}
-              className="absolute cursor-pointer"
+      {transitions((style, i) => {
+        const Page = pages[i];
+        return (
+          <Page>
+            <div
+              className="flex flex-wrap justify-center w-[300px] md:w-[500px] mb-20"
+              onClick={handleClick}
             >
-              {itemsFront[i]}
-            </animated.div>
-            <animated.div
-              style={{
-                transform: rotateX.to(
-                  (val) => `perspective(600px) rotateX(${180 - val}deg)`
-                ),
-                transformStyle: 'preserve-3d',
-                backfaceVisibility: 'hidden',
-              }}
-              className="absolute cursor-pointer"
-            >
-              {items[i]}
-            </animated.div>
-          </div>
-        ))}
-      </div>
+              {trail.map(({ rotateX }, i) => (
+                <div
+                  key={i}
+                  className={`flex justify-center items-center w-12 h-12 md:w-16 md:h-16 relative ml-0.5 md:ml-2 mb-2 text-3xl  text-white rounded-full ${
+                    isFlipped.current ? 'bg-blue-300' : 'bg-blue-200'
+                  }`}
+                >
+                  <animated.div
+                    key={items[i]}
+                    style={{
+                      transform: rotateX.to(
+                        (val) => `perspective(600px) rotateX(${val}deg)`
+                      ),
+                      transformStyle: 'preserve-3d',
+                      backfaceVisibility: 'hidden',
+                    }}
+                    className="absolute cursor-pointer"
+                  >
+                    {itemsFront[i]}
+                  </animated.div>
+                  <animated.div
+                    style={{
+                      transform: rotateX.to(
+                        (val) => `perspective(600px) rotateX(${180 - val}deg)`
+                      ),
+                      transformStyle: 'preserve-3d',
+                      backfaceVisibility: 'hidden',
+                    }}
+                    className="absolute cursor-pointer"
+                  >
+                    {items[i]}
+                  </animated.div>
+                </div>
+              ))}
+            </div>
+          </Page>
+        );
+      })}
     </div>
   );
 };
