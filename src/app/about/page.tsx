d28d85ecd59e08f21d82051435fc9e09f1a0c45d
ginsum <immime12697@gmail.com>
2024-05-08 16:50:02 +0900

    feat: login, about 페이지 next로 마이그레이션

---
+++ src/app/about/page.tsx
@@ -0,0 +1,149 @@
+import { Suspense } from "react";
+import Card from "src/components/Card";
+import Loading from "src/components/Loading";
+import { getInfo } from "src/firebase/info";
+
+export default async function AboutPage() {
+  const resumeData = await getInfo();
+
+  const { introduction, contact, experience, skill, education, etc } =
+    resumeData || {};
+
+  return (
+    <Suspense fallback={<Loading />}>
+      <div className="flex flex-col justify-center container mx-auto w-full md:w-[680px] px-5 md:px-6 pt-10 pb-28 tracking-tight ">
+        <div className="text-2xl font-bold mb-4 text-blue-900">About</div>
+        <div className="">
+          <div className="text-xl font-medium mb-1 text-zinc-800">
+            {introduction?.title}
+          </div>
+          <div className="whitespace-pre-wrap text-zinc-700 font-light ">
+            {introduction?.description}
+          </div>
+        </div>
+        <div className="my-6 text-sm">
+          <div className="text-zinc-600">
+            <span className="mr-4 font-semibold text-blue-400">Github</span>
+            <a href={contact?.github} target="_blank" rel="noreferrer">
+              <span>{contact?.github}</span>
+            </a>
+          </div>
+          <div className="text-zinc-600">
+            <span className="mr-6 font-semibold text-blue-400">Email</span>
+            <span>{contact?.email}</span>
+          </div>
+        </div>
+        <Card>
+          <div className="">
+            <div className="text-lg font-light mb-4 text-blue-600">경험</div>
+            <div className="flex flex-col dt:flex-row ">
+              <div className="flex justify-between mb-4 md:mb-8">
+                <div className="flex flex-col">
+                  <span className="text-xl font-semibold text-zinc-700">
+                    {experience?.company}
+                  </span>
+                  <span className="text-zinc-800 font-light">
+                    {experience?.description}
+                  </span>
+                </div>
+
+                <span className="text-zinc-600 font-light">
+                  {experience?.date}
+                </span>
+              </div>
+              <div className="">
+                {experience?.detail.map((detail: any) => (
+                  <div key={detail.title} className="mb-8 md:mb-12">
+                    <div className="text-lg font-medium mb-1 text-blue-800">
+                      {detail.title}
+                    </div>
+                    <hr />
+                    <div className="text-base mt-3 ml-3">
+                      {detail.content.map((el: string, index: number) => (
+                        <div
+                          key={el}
+                          className={`${index === 1 ? "mb-3" : "mb-0.5"} ${
+                            index < 2
+                              ? "font-normal text-zinc-600"
+                              : "font-light text-zinc-800"
+                          }`}
+                        >
+                          {el}
+                        </div>
+                      ))}
+                    </div>
+                  </div>
+                ))}
+                <hr />
+                <div className="mt-4 text-zinc-700 font-normal">
+                  {experience?.subDetail.map((el: string) => (
+                    <div key={el} className="mb-1">
+                      {el}
+                    </div>
+                  ))}
+                </div>
+              </div>
+            </div>
+          </div>
+        </Card>
+        <Card>
+          <div className="flex flex-col md:flex-row">
+            <div className="w-20 text-lg font-light mr-4 text-blue-700">
+              기술
+            </div>
+            <div className="flex flex-col">
+              <div className="flex flex-wrap mb-2">
+                {skill?.stack.map((el: string) => (
+                  <span
+                    key={el}
+                    className="mr-1 mb-2 px-3 py-1 bg-zinc-400 rounded-xl text-sm text-white font-light"
+                  >
+                    {el}
+                  </span>
+                ))}
+              </div>
+              <div className="flex flex-wrap">
+                {skill?.tool.map((el: string) => (
+                  <span
+                    key={el}
+                    className="mb-2 mr-1 px-3 py-1 bg-slate-400 rounded-xl text-sm text-white font-light"
+                  >
+                    {el}
+                  </span>
+                ))}
+              </div>
+            </div>
+          </div>
+        </Card>
+        <Card>
+          <div className="flex flex-col md:flex-row">
+            <div className="w-20 text-lg font-light mb-2 text-blue-700">
+              교육
+            </div>
+            {education && (
+              <div className="flex flex-col text-zinc-700">
+                <div className="mb-2">
+                  <span className="mr-2">{education[1][0]}</span>
+                  <span className="text-xs">{education[1][1]}</span>
+                </div>
+                <div>
+                  <span className="mr-2">{education[2][0]}</span>
+                  <span className="text-xs">{education[2][1]}</span>
+                </div>
+              </div>
+            )}
+          </div>
+        </Card>
+        <Card>
+          <div className="flex flex-col md:flex-row">
+            <div className="w-20 text-lg font-light text-blue-700">기타</div>
+            <div className="flex items-center text-zinc-700">
+              <span className="mr-2">{etc && etc[1][0]}</span>
+              <span className="text-xs">{etc && etc[1][1]}</span>
+            </div>
+          </div>
+        </Card>
+      </div>
+    </Suspense>
+  );
+}
